3efaafcc46b1597fadf3dff02451b88df7f9ba95
Faisal6454 <faisal.teamkoder@gmail.com>
2024-07-03 16:50:17 +0530

    frist comment

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { DataProvider } from "./context/Context";
 import Layout from "./components/shared/Layout";
 import Login from "./pages/login/Login";
 import Otp from "./pages/Otp/OTP";
-import Dashboard from "./pages/dashboard/Dashboard";
+// import Dashboard from "./pages/dashboard/Dashboard";
 import Company from "./pages/company/Company";
 import Tools from "./pages/tools/Tools";
 
@@ -26,7 +26,7 @@ function App() {
                 <Route path="/" element={<Login />} />
                 <Route path="/otp" element={<Otp />} />
                 <Route path="/layout" element={<Layout />}>
-                  <Route index element={<Dashboard />} />
+                  {/* <Route index element={<Dashboard />} /> */}
                   <Route path="company" element={<Company />} />
                   <Route path="tools" element={<Tools />} />
                 </Route>
