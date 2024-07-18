6c548c9b458a52ad7e7f004965d034d9089f716f
Faisal6454 <faisal.teamkoder@gmail.com>
2024-07-18 13:13:11 +0530

    "ForgotPassword page done "

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -49,7 +49,7 @@ const LoginForm = () => {
         <div className="right-box">
           <div className="header-text">
             <img src={Logo} alt="logo" className="logo" />
-            <h2>SOC Login</h2>
+            <h1>SOC Login</h1>
             <p>Enter your details below</p>
           </div>
           <Formik
--- src/pages/password/ForgotPassword.jsx
+++ src/pages/password/ForgotPassword.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Style.css";
 import img from "../../assets/images/forgotpassword.png";
 import logo from "../../assets/images/Logo.png";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { IoArrowBack } from "react-icons/io5";
+import { useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
+
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+
+ 
+
+  const initialValues = {
+    email: "",
+  };
+
+  const validationSchema = Yup.object({
+    email: Yup.string().email("Invalid email format").required("Required"),
+
+  });
+
+  const onSubmit = (values) => {
+    console.log("Form data", values);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Assume login is successful
+    navigate("/otp");
+  };
+
   return (
     <div className="container">
       <div className="row">
@@ -12,17 +40,45 @@ const ForgotPassword = () => {
           <img src={img} alt="Forgot Password" />
         </div>
         <div className="right">
-          <img src={logo} alt="soc-logo" />
-          <h1>Forgot Password</h1>
-          <p>Enter your details below</p>
-          <div>
-            <input type="email" placeholder="Email" />
+          <div className="right-header">
+            <img src={logo} alt="soc-logo" className="logo" />
+            <h1>Forgot Password</h1>
+            <p>Enter your details below</p>
           </div>
-          <button className="btn">Send OTP</button>
-          <button type="submit" className="btn back-btn">
-            <IoArrowBack size={25} />
-            Back
-          </button>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {() => (
+              <Form onSubmit={handleLogin}>
+                <div className="input-group">
+                  <Field
+                    type="email"
+                    id="email"
+                    name="email"
+                    placeholder="Email Address"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    className="form-control"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="error"
+                  />
+                </div>
+                <div className="btn-group">
+                  <button className="btn">Send OTP</button>
+                  <button type="submit" className="btn back-btn">
+                    <IoArrowBack size={25} />
+                    Back
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
